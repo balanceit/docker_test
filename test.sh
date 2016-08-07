72c674f2382af13207e0c15034c02f423ef2915b
David <balance.it@gmail.com>
2016-08-07 22:27:41 +1200

    docker with login 4

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-curl localhost:8000
-curl 0.0.0.0:8000
-curl 127.0.0.1:8000
 status=$(curl --write-out "%{http_code}\n" --silent --output /dev/null localhost:8000)
 
 if [ $status -ne 200 ]; then
