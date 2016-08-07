8552e269e6928c1c3f974402c39fdca3e5d042c6
David <balance.it@gmail.com>
2016-08-07 20:01:57 +1200

    random commit message 24

--- db/migrations/1_create_list_table.sql
+++ db/migrations/1_create_list_table.sql
@@ -1,8 +1,6 @@
 
 -- +migrate Up
-create table list (
-	currenttime timestamp not null default current_timestamp
-);
+create table list (currenttime timestamp not null default current_timestamp);
 
 -- +migrate Down
 DROP TABLE list;
